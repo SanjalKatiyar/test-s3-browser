dce7804d13bde9455139e8f654b4798339536f44
SanjalKatiyar <sanjaldhir@gmail.com>
2024-08-14 12:46:20 +0530

    test

--- list_objects.js
+++ list_objects.js
@@ -3,7 +3,8 @@ const https = require('https');
 const { NodeHttpHandler } = require('@aws-sdk/node-http-handler');
 const { accessKeyId, secretAccessKey, endpoint, region } = require('./creds');
 
-// SDK gives a maximum of 1000 files list by default, in order to get more than that, check: https://stackoverflow.com/a/69754448
+// SDK gives a maximum of 1000 files list by default, in order to get more than that, check: https://stackoverflow.com/a/69754448, https://stackoverflow.com/a/57540786 & https://stackoverflow.com/a/18324270 
+// consider using "listObjectsV2" (if needed): https://stackoverflow.com/a/37539994
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#listObjects-property
 
 const client = new S3({
